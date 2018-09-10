ed82011f9d50a8c585c09e8372405124363d38c1
zs1843 <zhandrea1843@qq.com>
2018-09-10 15:23:31 +0800

    'init'

---
+++ src/components/VtxDemo/VtxDemo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './VtxDemo.css';
+
+function VtxDemo() {
+  return (
+    <div className={styles.normal}>
+      Component: VtxDemo
+    </div>
+  );
+}
+
+export default VtxDemo;
